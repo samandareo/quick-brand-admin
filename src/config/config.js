ecf0a081529c2091f88adac3659b3402c7c1d874
samandareo <smdeveloper04@gmail.com>
2025-07-26 18:21:04 +0500

    Added port

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: import.meta.env.VITE_BACKEND || "http://31.220.51.218",
+  apiUrl: import.meta.env.VITE_BACKEND || "http://31.220.51.218:3001",
 };
 
 export default config;
